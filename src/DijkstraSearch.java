ab9d2ba6c9c7031dbe5021e3ba4f3c05e2611da5
damirkairlbay <154755310+damirkairlbay@users.noreply.github.com>
2024-05-29 23:57:50 +0500

    Update DijkstraSearch.java

--- src/DijkstraSearch.java
+++ src/DijkstraSearch.java
@@ -1,2 +1,34 @@
-public class DijkstraSearch {
+import java.util.*;
+
+public class DijkstraSearch<V> {
+    public void dijkstra(Vertex<V> start, WeightedGraph<V> graph) {
+        Map<Vertex<V>, Double> distances = new HashMap<>();
+        PriorityQueue<Vertex<V>> pq = new PriorityQueue<>(Comparator.comparing(distances::get));
+        Map<Vertex<V>, Vertex<V>> predecessor = new HashMap<>();
+        Set<Vertex<V>> visited = new HashSet<>();
+
+        for (Vertex<V> v : graph.map.keySet()) {
+            distances.put(v, Double.MAX_VALUE);
+        }
+        distances.put(start, 0.0);
+        pq.add(start);
+
+        while (!pq.isEmpty()) {
+            Vertex<V> current = pq.poll();
+            visited.add(current);
+
+            for (Map.Entry<Vertex<V>, Double> adj : current.getAdjacentVertices().entrySet()) {
+                Vertex<V> neighbor = adj.getKey();
+                double weight = adj.getValue();
+                if (!visited.contains(neighbor)) {
+                    double newDist = distances.get(current) + weight;
+                    if (newDist < distances.get(neighbor)) {
+                        distances.put(neighbor, newDist);
+                        predecessor.put(neighbor, current);
+                        pq.add(neighbor);
+                    }
+                }
+            }
+        }
+    }
 }
