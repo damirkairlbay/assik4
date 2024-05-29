d462566e6d103dac6812e5f88a45af6e9d867f5f
damirkairlbay <154755310+damirkairlbay@users.noreply.github.com>
2024-05-29 23:47:55 +0500

    Update BreadthFirstSearch.java

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -1,2 +1,22 @@
-public class BreadthFirstSearch {
+import java.util.*;
+
+public class BreadthFirstSearch<V> {
+    public void bfs(Vertex<V> start) {
+        Queue<Vertex<V>> queue = new LinkedList<>();
+        Set<Vertex<V>> visited = new HashSet<>();
+
+        queue.add(start);
+        visited.add(start);
+
+        while (!queue.isEmpty()) {
+            Vertex<V> vertex = queue.poll();
+            System.out.println("Visited: " + vertex.getData());
+            for (Vertex<V> v : vertex.getAdjacentVertices().keySet()) {
+                if (!visited.contains(v)) {
+                    visited.add(v);
+                    queue.add(v);
+                }
+            }
+        }
+    }
 }
